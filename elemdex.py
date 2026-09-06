471f83adc5b95072fcd8607d361171d2b01922d1
cvetyshayasiren <75530391+cvetyshayasiren@users.noreply.github.com>
2026-09-06 17:58:02 +0000

    dev

--- elemdex.py
+++ elemdex.py
@@ -1,23 +1,25 @@
-import json
-import random
-import sys
+import argparse
 
-from utils import pretty_element
+from find import element_by_name, element_by_number, random_element
 
 def main():
+    parser = argparse.ArgumentParser(description= "Program for working with chemical elements")
 
-    if len(sys.argv) != 2:
-        print("-p ")
-        sys.exit(1)
+    parser.add_argument('-r', '--random', action='store_true', help='Return a random element')
+    parser.add_argument('element', nargs='?', default=None, help='Element name or number')
+    args = parser.parse_args()
 
-    with open("data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
+    if args.random or args.element is None:
+        print(random_element()); return
 
-    print(sys.argv[0])
+    element_input = args.element
+
+    if element_input.isdigit():
+        print(element_by_number(element_input)); return 
+    else:
+        print(element_by_name(element_input)); return
+    
 
 
-    element = random.choice(data)
-    # print(pretty_element(element))
-
-
-main()
+if __name__ == "__main__":
+    main()
